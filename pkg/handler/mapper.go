2c3606f53810cb7cf5a99e2a1f3f3960e69330ff
Michael <machado.m3@icloud.com>
2024-06-09 09:48:37 -0300

    feat: new handler called mapper to handle mappings.go. Before save a new template we need to create an alias in mapping.go

---
+++ pkg/handler/mapper.go
@@ -0,0 +1,71 @@
+package handler
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type InfoMap struct {
+	Path     string `json:"path"`
+	CreateAt string `json:"createAt"`
+}
+
+var magicMap map[string]InfoMap
+var fullPath string
+
+func initMapping() {
+	filename := "internal/mapping.json"
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+
+	fullPath = filepath.Join(path, filename)
+
+	file, err := os.Open(fullPath)
+	if err != nil {
+		log.Fatal("Error on open file", err)
+	}
+
+	defer file.Close()
+
+	jsonData, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalln("Error when try read file.")
+		return
+	}
+
+	err = json.Unmarshal(jsonData, &magicMap)
+	if err != nil {
+		log.Fatalln("Error when try convert file to map.", err)
+		return
+	}
+
+}
+
+func setNewAlias(alias, path string) {
+	layout := "2006-01-02 15:04:05"
+	formattedTime := time.Now().Format(layout)
+
+	info := InfoMap{
+		path,
+		formattedTime,
+	}
+
+	magicMap[alias] = info
+	updatedJSON, err := json.MarshalIndent(magicMap, "", " ")
+	if err != nil {
+		return
+	}
+
+	err = os.WriteFile(fullPath, updatedJSON, 0644)
+	if err != nil {
+		log.Fatal("Cannot write in file")
+		return
+	}
+
+}
--- pkg/handler/memorizer.go
+++ pkg/handler/memorizer.go
@@ -1,13 +1,7 @@
 package handler
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"os"
-	"path/filepath"
-	"time"
 )
 
 var destination string = "../../templates/"
@@ -15,8 +9,8 @@ var destination string = "../../templates/"
 func MemorizeTemplate(alias, path string) {
 	fmt.Printf("path: %s", path)
 
-	f1("internal/mapping.json")
-	// setNewAlias(alias, "caminhos")
+	initMapping()
+	setNewAlias(alias, "caminhos")
 
 	// err := os.WriteFile(path+alias, []byte("hello, Gophers!"), 0666)
 	// if err != nil {
@@ -48,67 +42,4 @@ func MemorizeTemplate(alias, path string) {
 func copyFile(src string) error {
 	fmt.Printf("%v %v ", src, destination)
 	return nil
-}
-
-type InfoMap struct {
-	Path     string    `json:"path"`
-	CreateAt time.Time `json:"createAt"`
-}
-
-var magicMap map[string]InfoMap
-var fullPath string
-
-func f1(filename string) {
-	path, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	fmt.Println("--------------")
-	fmt.Println(time.Now())
-	fmt.Println("--------------")
-	fullPath = filepath.Join(path, filename)
-
-	file, err := os.Open(fullPath)
-	if err != nil {
-		log.Fatal("Error on open file", err)
-	}
-
-	defer file.Close()
-
-	jsonData, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatalln("Error when try read file.")
-		return
-	}
-
-	fmt.Printf("%s", jsonData)
-
-	err = json.Unmarshal(jsonData, &magicMap)
-	if err != nil {
-		log.Fatalln("Error when try convert file to map.", err)
-		return
-	}
-
-}
-
-func setNewAlias(alias, path string) {
-	info := InfoMap{
-		path,
-		time.Now(),
-	}
-
-	fmt.Println("alias: ", alias)
-	magicMap[alias] = info
-	updatedJSON, err := json.MarshalIndent(magicMap, "", " ")
-	if err != nil {
-		return
-	}
-
-	err = os.WriteFile(fullPath, updatedJSON, 0644)
-	if err != nil {
-		log.Fatal("Cannot write in file")
-		return
-	}
-
 }
